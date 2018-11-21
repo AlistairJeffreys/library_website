67b9d625a6d68db3df3ac8bdca5d14d7533f1265
Alistair Jeffreys <alistair.jeffreys@bt.com>
2018-11-21 16:30:12 +0000

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
   end
   
+  test "sign up on the home page" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_template 'users/new'
+  end
+  
 end
