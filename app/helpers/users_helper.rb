fb2064aee3fe779eb6246b11bef8f3c2f5aacec6
Alistair Jeffreys <alistair.jeffreys@bt.com>
2018-12-05 10:21:00 +0000

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  
+  # Returns a default profile image for a user
+  def user_icon
+    image_tag("users/user_icon.png", alt: "user icon", class: "avatar")
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(name: "Test User", email: "test@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  
   test "should get new" do
     get signup_path
+    assert_response :success
+  end
+  
+  test "should get show" do
+    get "/users/1"
     assert_response :success
   end
 
